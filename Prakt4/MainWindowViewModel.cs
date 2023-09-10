6cc08d471d1cba73ff3221e7e3f8ec66c25f3a27
Mesery <Meserygamer@gmail.com>
2023-09-11 00:54:59 +0300

    Пофикшены баги, изменен способ расчета прошедшего времени с момента рождения, добавлена документация

--- Prakt4/MainWindowViewModel.cs
+++ Prakt4/MainWindowViewModel.cs
@@ -13,11 +13,23 @@ using System.Windows.Data;
 
 namespace Prakt4
 {
+    /// <summary>
+    /// ViewModel главного окна
+    /// </summary>
     class MainWindowViewModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Хранит дату выбранную пользователем
+        /// </summary>
         private DateTime? _userSelectedBirthday;
+        /// <summary>
+        /// Хранит сегодняшнюю дату (Нужен для ограничения календаря)
+        /// </summary>
         private DateTime _nowDateTime;
 
+        /// <summary>
+        /// Свойство для получения выбранной пользователем даты
+        /// </summary>
         public DateTime? UserSelectedBirthday
         {
             get => _userSelectedBirthday;
@@ -29,6 +41,10 @@ namespace Prakt4
                 OnPropertyChanged("NumberOfLeapYearsExperienced");
             }
         }
+
+        /// <summary>
+        /// Свойство для получения текущей даты
+        /// </summary>
         public DateTime NowDateTime
         {
             get { 
@@ -41,6 +57,9 @@ namespace Prakt4
             }
         }
 
+        /// <summary>
+        /// Хранит количество дней рождений отпразднованных в день недели рождения человека
+        /// </summary>
         public int NumberOfBirthdaysCelebratedOnTheBirthday
         {
             get
@@ -58,6 +77,9 @@ namespace Prakt4
             }
         }
 
+        /// <summary>
+        /// Хранит количество прошедших весокосных лет с рождения человека
+        /// </summary>
         public string NumberOfLeapYearsExperienced
         {
             get
@@ -80,26 +102,36 @@ namespace Prakt4
             }
         }
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
         public MainWindowViewModel()
         {
             NowDateTime = DateTime.Now;
         }
 
+        #region реализация INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
+        #endregion
     }
 
+    /// <summary>
+    /// Конвертер для получения количества прошедших лет
+    /// </summary>
     class CalculationOfTheNumberOfYearsPassedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if(value is null) return null;
-            string result = (DateTime.Now.Year - ((DateTime)value).Year).ToString();
-            switch(result[result.Length - 1])
+            string result;
+            if (((DateTime)value).DayOfYear <= DateTime.Today.DayOfYear) result = (DateTime.Today.Year - ((DateTime)value).Year).ToString();
+            else result = (DateTime.Today.Year - ((DateTime)value).Year - 1).ToString();
+            switch (result[result.Length - 1])
             {
                 case '1':
                     if (Regex.IsMatch(result, @"^.*(11|12|13|14)$"))
@@ -132,12 +164,22 @@ namespace Prakt4
         }
     }
 
+    /// <summary>
+    /// Конвертер для получения количества прошедших месяцев
+    /// </summary>
     class CalculationOfTheNumberOfMonthPassedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is null) return null;
-            string result = ((DateTime.Now.Year - ((DateTime)value).Year) * 12 + (DateTime.Now.Month - ((DateTime)value).Month)).ToString();
+            int Age;
+            if (((DateTime)value).DayOfYear <= DateTime.Today.DayOfYear) Age = (DateTime.Today.Year - ((DateTime)value).Year);
+            else Age = (DateTime.Today.Year - ((DateTime)value).Year - 1);
+            string result;
+            if (DateTime.Today.Day - ((DateTime)value).Day < 0) result = (DateTime.Today.Month - ((DateTime)value).AddYears(Age).Month - 1 + 
+                    (DateTime.Today.Year - ((DateTime)value).AddYears(Age).Year) * 12).ToString();
+            else result = (DateTime.Today.Month - ((DateTime)value).AddYears(Age).Month +
+                    (DateTime.Today.Year - ((DateTime)value).AddYears(Age).Year) * 12).ToString();
             switch (result[result.Length - 1])
             {
                 case '1':
@@ -171,12 +213,23 @@ namespace Prakt4
         }
     }
 
+    /// <summary>
+    /// Конвертер для получения количества прошедших дней
+    /// </summary>
     class CalculationOfTheNumberOfDaysPassedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is null) return null;
-            string result = Math.Truncate((DateTime.Now - (DateTime)value).TotalDays).ToString();
+            int Age;
+            if (((DateTime)value).DayOfYear <= DateTime.Today.DayOfYear) Age = (DateTime.Today.Year - ((DateTime)value).Year);
+            else Age = (DateTime.Today.Year - ((DateTime)value).Year - 1);
+            int month;
+            if (DateTime.Today.Day - ((DateTime)value).Day < 0) month = (DateTime.Today.Month - ((DateTime)value).AddYears(Age).Month - 1 +
+                    (DateTime.Today.Year - ((DateTime)value).AddYears(Age).Year) * 12);
+            else month = (DateTime.Today.Month - ((DateTime)value).AddYears(Age).Month +
+                    (DateTime.Today.Year - ((DateTime)value).AddYears(Age).Year) * 12);
+            string result = (DateTime.Today - ((DateTime)value).AddYears(Age).AddMonths(month)).TotalDays.ToString();
             switch (result[result.Length - 1])
             {
                 case '1':
@@ -210,6 +263,9 @@ namespace Prakt4
         }
     }
 
+    /// <summary>
+    /// Конвертер для получения дня недели в который родился человек
+    /// </summary>
     class CalculateDayOfBirthDay : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
